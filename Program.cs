41c4e997b7f5fcc9ab01f8ba35b5c6d4bd2db357
Swaraa1211 <saiswaroopasaranath1211@gmail.com>
2023-04-11 23:51:54 +0530

    Added File

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+
+namespace BalancedParantheses
+{
+    internal class Program
+    {
+        public static ArrayList paranthesesList = new ArrayList();
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            Console.Write("Enter Number: ");
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+            obj.paranthesesGenerate(inputNumber);
+
+            foreach(var paranthese in paranthesesList)
+            {
+                Console.Write($"{paranthese} ");
+            }
+        }
+        public ArrayList paranthesesGenerate(int input)
+        {
+            generateParantheses(paranthesesList, 0, 0, input, "");
+            return paranthesesList;
+        }
+        public void generateParantheses(ArrayList paranthesesList, int open, int close, int input, string str)
+        {
+
+            if (open == input && close == input)
+            {
+                paranthesesList.Add(str);
+                return;
+            }
+            if (open < input)
+            {
+                generateParantheses(paranthesesList, open + 1, close, input, str + "(");
+            }
+            if (close < open)
+            {
+                generateParantheses(paranthesesList, open, close + 1, input, str + ")");
+
+            }
+        }
+    }
+}
